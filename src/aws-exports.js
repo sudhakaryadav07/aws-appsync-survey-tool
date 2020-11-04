3c038f44ccaaf8a3f328e551ec77de155b5e6c26
ShraddhaB <sbhingarde2017@gmail.com>
2020-11-04 14:15:25 +0530

    graphql schema added

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,10 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_rwVu9J1ir",
     "aws_user_pools_web_client_id": "5ve0qvov36i438thqpgituksge",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://mow4xyugejdbtdkexjd2qwxrsi.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
 
 
